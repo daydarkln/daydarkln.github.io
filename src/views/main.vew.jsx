531ebecb1a5fe81146fd9a727e7e5ae027419796
Denis <69271897+DelinFix@users.noreply.github.com>
2022-11-11 12:23:51 +0300

    Refactor Main-view

--- src/views/main.vew.jsx
+++ src/views/main.vew.jsx
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
 
+// Мы перенесли данные о пунктах меню сюда из App.jsx, сделать название - MainPage и перенести ее в src/pages
+// опсиать props
 const MainView = (props) => {
+  //сделать деструктуризацию props
   return (
     <div className="df fdc">
       <h1>Шпион</h1>
       <ul mode="inline" className="main-view">
+    //применить новый компонент List
         {props.menuItems.map((item) => (
           <li key={item.id}>
             <Link to={item.path}>{item.name}</Link>
